9930c6a18388e1f0cffad1571cbe234c2427f7f2
Nuclear-Fox <grid30@mail.ru>
2025-07-02 17:12:54 +0300

    add consumer

--- practice/consumer-service/build.gradle.kts
+++ practice/consumer-service/build.gradle.kts
@@ -14,7 +14,9 @@ dependencies {
 
     implementation ("org.slf4j:slf4j-api:2.0.13")
     implementation ("ch.qos.logback:logback-classic:1.5.6")
-
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.test {
--- practice/consumer-service/src/main/java/com/prosoft/KafkaConsumerApp.java
+++ practice/consumer-service/src/main/java/com/prosoft/KafkaConsumerApp.java
@@ -1,6 +1,7 @@
 package com.prosoft;
 
 import com.prosoft.config.KafkaConfig;
+import com.prosoft.domain.Symbol;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -11,24 +12,64 @@ import java.time.Duration;
 import java.util.Collections;
 
 /**
- * Webinar-01: Kafka consumer-service
+ * Webinar-03: Kafka consumer-service (прием экземпляров класса Person из topic2)
  * Использования метода consumer.poll().
  */
 public class KafkaConsumerApp {
-
     private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerApp.class);
     private static final Duration TEN_MILLISECONDS_INTERVAL = Duration.ofMillis(10);
 
     public static void main(String[] args) {
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(KafkaConfig.getConsumerConfig());
-        try (consumer) {
-            consumer.subscribe(Collections.singletonList(KafkaConfig.TOPIC));
-            while (true) {
-                ConsumerRecords<String, String> consumerRecords = consumer.poll(TEN_MILLISECONDS_INTERVAL);
-                for (ConsumerRecord<String, String> cr : consumerRecords) {
-                    logger.info("topic = {}, offset = {}, key = {}, value = {}", cr.topic(), cr.offset(), cr.key(), cr.value());
+
+        /***
+         * Создание и запуск консюмеров в трех потоках
+         */
+        for (int i = 0; i < KafkaConfig.NUM_CONSUMERS; i++) {
+
+            new Thread(() -> {
+                KafkaConsumer<Long, Symbol> consumerVowels = new KafkaConsumer<>(KafkaConfig.getConsumerConfig());
+
+                consumerVowels.subscribe(Collections.singletonList(KafkaConfig.TOPIC_VOWELS));
+
+
+                /** Возможно использовать try-with-resources */
+                try {
+                    while (true) {
+                        ConsumerRecords<Long, Symbol> records = consumerVowels.poll(TEN_MILLISECONDS_INTERVAL);
+                        for (ConsumerRecord<Long, Symbol> cr : records) {
+                            logger.info("Consumer {}. Received message: key={}, value={}, partition={}, offset={}",
+                                    Thread.currentThread().getName(), cr.key(), cr.value(), cr.partition(), cr.offset());
+                        }
+                    }
+                } catch (Exception e) {
+                    logger.error("Exception occurred in consumer thread", e);
+                } finally {
+                    consumerVowels.close();
                 }
-            }
+            }).start();
+
+//            new Thread(() -> {
+//                KafkaConsumer<Long, Symbol> consumerConsonants = new KafkaConsumer<>(KafkaConfig.getConsumerConfig());
+//
+//                consumerConsonants.subscribe(Collections.singletonList(KafkaConfig.TOPIC_CONSONANTS));
+//
+//
+//                /** Возможно использовать try-with-resources */
+//                try {
+//                    while (true) {
+//                        ConsumerRecords<Long, Symbol> records = consumerConsonants.poll(TEN_MILLISECONDS_INTERVAL);
+//                        for (ConsumerRecord<Long, Symbol> cr : records) {
+//                            logger.info("Consumer {}. Received message: key={}, value={}, partition={}, offset={}",
+//                                    Thread.currentThread().getName(), cr.key(), cr.value(), cr.partition(), cr.offset());
+//                        }
+//                    }
+//                } catch (Exception e) {
+//                    logger.error("Exception occurred in consumer thread", e);
+//                } finally {
+//                    consumerConsonants.close();
+//                }
+//            }).start();
+
         }
     }
 
